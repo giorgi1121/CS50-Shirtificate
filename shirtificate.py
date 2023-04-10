dd061bbd866ef668523803d0ecf0a0744bd488b6
Giorgi Abashidze <126471823+giorgi1121@users.noreply.github.com>
2023-04-11 03:01:49 +0400

    Update shirtificate.py

--- shirtificate.py
+++ shirtificate.py
@@ -7,20 +7,20 @@ pdf.add_page()
 pdf.set_auto_page_break(False)
 
 #set font for text and write it in the center of the top of the page
-pdf.set_font("Times", "B", 20)
+pdf.set_font("Times", "B", 50)
 pdf.cell(200, 20, "CS50 Shirtificate", 0, 0, align="C")
 
 #insert shirt image
 pdf.image("shirtificate.png", x=1, y=60)
 
 #create variable name which asks user for name
-name = str(input("What is your name: ")).capitalize
+name = str(input("What is your name: ")).title()
 
 #set new font and text color
-pdf.set_font("helvetica", "B", 40)
+pdf.set_font("helvetica", "B", 25)
 pdf.set_text_color(255, 255, 255)
 #write text on the shirt
-pdf.cell(-200, 280, f"{name} took CS50", align="C")
+pdf.cell(-210, 280, f"{name} took CS50", align="C")
 
 #output
 pdf.output("shirtificate.pdf")
